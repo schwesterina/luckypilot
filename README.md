2365b0aea72512073e62b99ef6c9eba17ed9f2ad
schwesterina <ina0394@hotmail.de>
2026-07-27 16:22:17 +0200

    LuckyPilot 2.0 Production Starter

---
+++ README.md
@@ -0,0 +1,28 @@
+# LuckyPilot 2.0 Production Starter
+
+Diese Version ist bewusst eine statische Webseite ohne npm-, pnpm-, React- oder
+TypeScript-Build. Dadurch kann sie sofort und zuverlässig über GitHub Pages
+veröffentlicht werden.
+
+## Veröffentlichung
+
+Der Workflow `.github/workflows/deploy-web.yml` veröffentlicht den Inhalt aus
+`site/`.
+
+## Enthalten
+
+- responsive LuckyPilot-Webseite
+- Suche und Kategorien
+- Favoriten
+- Teilnahme-Tracker
+- Gewinn-Markierung
+- mehrfach spielbare Aktionen
+- eigene Einträge
+- lokale Datensicherung
+- Offline-/PWA-Grundlage
+- Beispieldaten in `site/gewinnspiele.json`
+
+## Wichtig
+
+Die Beispieldaten müssen vor einer öffentlichen Bewerbung durch echte,
+zulässige Gewinnspielquellen ersetzt werden.
---
+++ site/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'luckypilot-v2-20260727';
+const ASSETS = ['./', './index.html', './gewinnspiele.json', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match('./index.html')))
+  );
+});
